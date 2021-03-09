b47d7dd555dbe17bb51ec2150e5a33fb9a75ee28
hoainv99 <55236988+hoainv99@users.noreply.github.com>
2021-03-09 13:58:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 #######################################################
-MODEL_DETECTOR = "weights/efficient_net_d0.bin"
-MODEL_OCR = "weights/multilines_ocr.pth"
+MODEL_DETECTOR = "weights_plate_number_detection/efficient_net_d0.bin"
+MODEL_OCR = "weights_plate_number_detection/multilines_ocr.pth"
 TMP = "tmp/"
 #######################################################
 FORMATE_SUPPORT = [".jpg", ".png", ".jpeg"]
